7b709d62d2567791b7a8759d97f5a7a68f0ed157
Jago Strong-Wright <jagoosw@protonmail.com>
2021-03-09 11:53:06 +0000

    checking error is the richt one

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from datetime import datetime
 from requests import HTTPError
 
 st.title("The Redboy's 777 Laps of St Legends in memory of Sam Fitzsimmons")
-try:
+#try:
+if True:
     data=pd.read_excel('https://drive.google.com/uc?id=1KOAFOCxiyeom2XjN7aDW4Sys1NYxjXqk&export=download',sheet_name='data_input').iloc[4:].drop(["Unnamed: 22","Unnamed: 23"],axis=1).fillna(0)
     total=[0,]+data.sum(axis=0).to_list()[1:]
     cumtot=[sum(total[:ind+1]) for ind,v in enumerate(total)]
@@ -53,8 +54,8 @@ try:
 
     fig = plot.get_figure()
     st.pyplot(fig)
-except:#I know this is bad practice but I can't work out what the HTTPError is actually called to just catch that
-    st.markdown("""Sorry our progress chart isn't available at the moment, Google doesn't like us downloading the data lots of times. Hopefully it will be available again later.""")
+#except:#I know this is bad practice but I can't work out what the HTTPError is actually called to just catch that
+#    st.markdown("""Sorry our progress chart isn't available at the moment, Google doesn't like us downloading the data lots of times. Hopefully it will be available again later.""")
 st.markdown("""In May of last year our beloved Captain EEE, Sam Fitzsimmons, passed away in May last year with Ewing's Sarcoma - a rare form of bone cancer.
 To raise money for the Bone Cancer Research Trust, the Garcons are undertaking a grueling challenge, bear crawling a cumulative 777 laps of the St. Legends pitch in 20+1 days begining the 1st of March.""")
 st.markdown("""To read our story, and for more details about the challenge, please see the link below:
